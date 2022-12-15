dbe8d439033d891aba8c57c8d9e4cd88727f8a60
DiegoPMacedo <di.macedo.90@gmail.com>
2022-12-15 10:21:10 -0300

    Update button-contator.js

--- SPA_basica_integracao/src/components/react/button-contator.js
+++ SPA_basica_integracao/src/components/react/button-contator.js
@@ -9,9 +9,3 @@ export const ButtonContator = () => {
     count
   );
 };
-
-// const ButtonContator = () => {
-//   const [count, setCount] = React.useState(0);
-//   const increment = () => setCount(count+1);
-//   return <button onClick={increment}>Contador: {count}</button>
-// }
